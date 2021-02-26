a0277c5dad9148710f7730a00445c791c4b84a27
Rodrigo Silva <rodrigosilvap93@gmail.com>
2021-02-25 22:22:21 -0300

    Secured Private Routes

--- src/App.js
+++ src/App.js
@@ -89,21 +89,21 @@ export default function App() {
         <CssBaseline />
         <Router>
           <Switch>
-            <Route exact path="/">
+            <PrivateRoute exact path="/">
               <Redirect to="/home" />
-            </Route> 
+            </PrivateRoute> 
             <Route exact path="/login">
               <Login />
             </Route>
             <Route exact path="/signup">
               <Signup />
             </Route>
-            <Route path="/home">
+            <PrivateRoute path="/home">
               <Home />
-            </Route>        
-            <Route exact path="/shift">
+            </PrivateRoute>        
+            <PrivateRoute exact path="/shift">
               <Shift />
-            </Route>
+            </PrivateRoute>
             <Route exact path="/recovery">
               <Recovery />
             </Route>
@@ -113,23 +113,4 @@ export default function App() {
       </ThemeProvider>
     </React.Fragment>
   );
-
-
-
-  /* <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> */
 }
